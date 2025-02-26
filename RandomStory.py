0d6889d0c619e7f10dc9f317589e8ae07da370cc
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-26 13:05:09 +0300

    drtgywerty

--- RandomStory.py
+++ RandomStory.py
@@ -1,18 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.json.ensure_ascii = False
+db = SQLAlchemy(app)
+
+
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
 
 @app.route('/')
-def main_view():
-    return 'Это главная страница!'
+def index_view():
+    stories = Story.query.all()
+    return jsonify([{str(story.id): story.text} for story in stories])
+
 
 @app.route('/add')
 def add_view():
     return 'Это страница для добавления рассказа'
 
-@app.route('/story')
-def story_view():
-    return 'Это страница со случайным рассказом'
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/story')
+def random_story_view():
+    return 'Это страница со случайным рассказом'
