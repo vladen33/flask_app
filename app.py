0ba10f1d1784a88ed57ead9f8ff54dce1a9792bb
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-27 23:05:33 +0300

    wertwerytwerty

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from random import randint
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'My super secret key: 3546dryefo58mgyo548h60974yu5'
 db = SQLAlchemy(app)
 
 
@@ -14,21 +19,41 @@ class Story(db.Model):
     text = db.Column(db.Text, nullable=False)
 
 
+class StoryForm(FlaskForm):
+    title = StringField(
+        'Название фильма',
+        validators=[DataRequired(message='Обязательное поле'), Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Название фильма',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    submit = SubmitField('Опубликовать')
+
+
 @app.route('/')
 def index_view():
     stories = Story.query.all()
     return render_template('stories.html', stories=stories)
 
-
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_view():
-    return 'Это страница добавления рассказа'
+    form = StoryForm()
+    if form.validate_on_submit():
+        story = Story(
+            title=form.title.data,
+            text=form.text.data
+        )
+        db.session.add(story)
+        db.session.commit()
+        return redirect(url_for('index_view'))
+    return render_template('add.html', form=form)
 
 
 @app.route('/story')
 def random_story_view():
     stories_count = Story.query.count()
-    random_story = Story.query.get(
+    random_story = Story.query.get_or_404(
         randint(1, stories_count)
     )
     return render_template('random.html', story=random_story)
