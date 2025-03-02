c0d3189456294de8236fe730de50de29589ebc46
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-02 17:03:36 +0300

    [Спринт 21/27 → Тема 3/4: REST API на Flask → Урок 4/8]

--- app/api.py
+++ app/api.py
@@ -6,11 +6,16 @@ from .models import Story
 
 @app.route('/api/stories/', methods=['GET'])
 def get_stories():
-    # ...
-    pass
+    stories = Story.query.all()
+    stories_list = [story.to_dict() for story in stories]
+    return jsonify(stories_list), 200
 
 
 @app.route('/api/add_story/', methods=['POST'])
 def add_story():
-    # ...
-    pass
+    data = request.get_json()
+    story = Story()
+    story.from_dict(data)
+    db.session.add(story)
+    db.session.commit()
+    return jsonify(story.to_dict())
--- app/models.py
+++ app/models.py
@@ -7,7 +7,14 @@ class Story(db.Model):
     text = db.Column(db.Text, nullable=False)
 
     def to_dict(self):
-        ...
+        return dict(
+            id = self.id,
+            title = self.title,
+            text = self.text
+        )
 
     def from_dict(self, data):
-        ...
+        for field in ['title', 'text']:
+            print('field = ', field)
+            if field in data:
+                setattr(self, field, data[field])
